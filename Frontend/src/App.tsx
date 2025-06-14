3f2ba33613405cedd075b11e2d24a9c5eed46320
biswarup-naha <jeeeeet6902@gmail.com>
2025-06-15 01:23:40 +0530

    login/signup ui made

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,19 @@
-
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+import AdminDashboard from './pages/AdminDashboard'
+import Register from './pages/Register'
 
 function App() {
 
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/admin-dashboard" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ Frontend/src/pages/Login.tsx
@@ -0,0 +1,38 @@
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    return (
+        <div className="w-full max-w-sm p-8 bg-white rounded-2xl shadow-[0_10px_30px_rgba(0,0,0,0.05)] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <h2 className="text-2xl font-semibold text-gray-800 mb-6 text-center">Login</h2>
+          <form className="space-y-5">
+            <div>
+              <label className="block mb-1 text-gray-600 text-sm">Email</label>
+              <input
+                type="email"
+                className="w-full px-4 py-2 bg-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-300"
+                placeholder="you@example.com"
+              />
+            </div>
+            <div>
+              <label className="block mb-1 text-gray-600 text-sm">Password</label>
+              <input
+                type="password"
+                className="w-full px-4 py-2 bg-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-300"
+                placeholder="••••••••"
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full py-2 bg-gray-800 text-white rounded-md shadow hover:bg-gray-700 transition"
+            >
+              Sign In
+            </button>
+          </form>
+          <p className="mt-6 text-sm text-center text-gray-500">
+            Don't have an account? <Link to={'/register'} className="text-gray-700 hover:underline">Sign up</Link>
+          </p>
+        </div>
+      );
+}
+
+export default Login
